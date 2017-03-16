71381d543c846d8cafcadbb9a2b2772e8936f80a
Jean-Marie Le Bonniec (externe - smile) <jm.lebonniec-e@meyclub.net>
2017-03-16 14:48:20 +0100

    add message to warn user to add the modified files to git

--- src/PhpGitHooks/Command/GoodJobLogo.php
+++ src/PhpGitHooks/Command/GoodJobLogo.php
@@ -29,6 +29,7 @@ final class GoodJobLogo
   @@@@@@@@@@   @@@@@@@@@@@@@@@@@@@
                  @@@@@@@@@@@@@@@
         </fg=yellow;options=bold;>\n
-        <fg=white;bg=yellow;options=bold;>       %s       </fg=white;bg=yellow;options=bold;>", $message);
+        <fg=white;bg=yellow;options=bold;>       %s       </fg=white;bg=yellow;options=bold;>\n
+        <fg=white;bg=blue;options=bold;>Warning: Add all modified files to git before launch again this command!!</fg=white;bg=blue;options=bold;>\n", $message);
     }
 }
